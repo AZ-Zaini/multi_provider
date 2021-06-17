f9ddb0cee9ba78c54e8bbe011f6554405a547d0b
akhmad-zaini <akhmadjaini97>
2021-06-17 16:27:19 +0800

    Adding Multiprovider

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:multi_provider/weight_provider.dart';
+import 'package:multi_provider/height_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,13 +21,19 @@ class HomePage extends StatelessWidget {
                 fontSize: 20,
               ),
             ),
-            Slider(
-              min: 1,
-              max: 100,
-              value: 40,
-              onChanged: (newValue) {
-                print('Weight : $newValue');
-              },
+            Consumer<WeightProvider>(
+              builder: (context, weightProvider, _) => Slider(
+                min: 1,
+                max: 100,
+                divisions: 100,
+                label: weightProvider.weight.round().toString(),
+                value: weightProvider.weight,
+                onChanged: (newValue) {
+                  newValue = newValue.roundToDouble();
+                  print('Weight : $newValue');
+                  weightProvider.weight = newValue;
+                },
+              ),
             ),
             SizedBox(
               height: 20,
@@ -33,26 +44,36 @@ class HomePage extends StatelessWidget {
                 fontSize: 20,
               ),
             ),
-            Slider(
-              min: 1,
-              max: 100,
-              value: 40,
-              activeColor: Colors.pink,
-              inactiveColor: Colors.pink.withOpacity(0.2),
-              onChanged: (newValue) {
-                print('Height : $newValue');
-              },
+            Consumer<HeightProvider>(
+              builder: (context, heightProvider, _) => Slider(
+                min: 1,
+                max: 200,
+                divisions: 200,
+                label: heightProvider.height.round().toString(),
+                value: heightProvider.height,
+                activeColor: Colors.pink,
+                inactiveColor: Colors.pink.withOpacity(0.2),
+                onChanged: (newValue) {
+                  newValue = newValue.roundToDouble();
+                  print('Height : $newValue');
+                  heightProvider.height = newValue;
+                },
+              ),
             ),
             SizedBox(
               height: 50,
             ),
-            Text(
-              'Your BMI:\n60.25',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w500,
+            Consumer<WeightProvider>(
+              builder: (context, weihtProvider, _) => Consumer<HeightProvider>(
+                builder: (context, heightProvider, _) => Text(
+                  'Your BMI:\n${(weihtProvider.weight / (pow(heightProvider.height / 100, 2))).toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
               ),
-              textAlign: TextAlign.center,
             )
           ],
         ),
